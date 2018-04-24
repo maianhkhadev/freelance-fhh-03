6d2ad87901e625a42d75c729998adfa8c2d13a54
Mai Anh Kha <maianhkha.dev@gmail.com>
2018-04-24 23:03:06 +0700

    done

--- hanoi/app/scripts/main.js
+++ hanoi/app/scripts/main.js
@@ -1,3 +1,10 @@
+$(document).ready(function() {
+  document.querySelector('#backToTop').addEventListener('click', function (e) {
+    e.preventDefault()
+    $('html, body').animate({ scrollTop: 0 })
+  })
+})
+
 function myFunction() {
   $('.nav').toggleClass('responsive')
 }
--- vietnam/app/scripts/main.js
+++ vietnam/app/scripts/main.js
@@ -1,3 +1,9 @@
+$(document).ready(function() {
+  document.querySelector('#backToTop').addEventListener('click', function (e) {
+    e.preventDefault()
+    $('html, body').animate({ scrollTop: 0 })
+  })
+})
 function myFunction() {
   $('.nav').toggleClass('responsive')
 }
